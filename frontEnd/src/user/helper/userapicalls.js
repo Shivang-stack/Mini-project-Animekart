e057d50b584546f7270fa9220032ffe874a03890
Shivang-stack <shivang1691401@gmail.com>
2022-03-27 23:04:54 +0530

    intial commit

---
+++ frontEnd/src/user/helper/userapicalls.js
@@ -0,0 +1,14 @@
+import { API } from "../../backend";
+
+//get a user
+
+export const getUserById = userId => {
+    return fetch(`${API}/user/${userId}`, {
+      method: "GET"
+    })
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => console.log(err));
+  };
+  
