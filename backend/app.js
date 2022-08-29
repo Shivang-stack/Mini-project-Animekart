1aa5466d48a60dc098e49d95834d5cf3ac0260fb
Shivang-stack <shivang1691401@gmail.com>
2022-08-29 18:47:19 +0530

    thrid commit

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,11 @@ const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
+const { application } = require("express");
 
 //DB Connection
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect("mongodb+srv://muserad:<password>@cluster0.lcttx.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -31,6 +32,10 @@ app.use(cookieParser());
 app.use(cors());
 
 //My Routes
+app.get("/", (req, res) => {
+  res.send("Animekart apis");
+});
+
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
